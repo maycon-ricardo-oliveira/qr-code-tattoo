5f863cbdf76652d918f6814c467c09e636346645
Maycon Oliveira <rick.maycon.98@gmail.com>
2023-08-15 13:31:14 -0300

    BUILD

--- src/icons/Like.tsx
+++ src/icons/Like.tsx
@@ -3,13 +3,13 @@ const Dislike = () => {
     <svg width="74" height="74" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
     <g id="like-button">
     <g id="Ellipse 1" filter="url(#filter0_d_99_193)">
-    <circle cx="37" cy="36" r="29" stroke="#3CDBA7" stroke-width="2"/>
+    <circle cx="37" cy="36" r="29" stroke="#3CDBA7" strokeWidth="2"/>
     </g>
     <path id="Vector" d="M46.9617 27.0998C46.3259 26.435 45.5698 25.9073 44.7368 25.547C43.9039 25.1866 43.0106 25.0007 42.1082 25C40.4014 25.0003 38.7569 25.6659 37.5003 26.8652C36.2438 25.6657 34.5993 25.0001 32.8924 25C31.989 25.001 31.0946 25.1874 30.2609 25.5486C29.4271 25.9098 28.6704 26.4386 28.0343 27.1046C25.321 29.9334 25.3221 34.3581 28.0366 37.175L36.0598 45.5046C36.8465 46.3213 38.1541 46.3213 38.9407 45.5046L46.964 37.175C49.6785 34.3581 49.6796 29.9334 46.9617 27.0998Z" fill="#3CDBA7"/>
     </g>
     <defs>
-    <filter id="filter0_d_99_193" x="0" y="0" width="74" height="74" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+    <filter id="filter0_d_99_193" x="0" y="0" width="74" height="74" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+    <feFlood floodOpacity="0" result="BackgroundImageFix"/>
     <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
     <feOffset dy="1"/>
     <feGaussianBlur stdDeviation="3.5"/>
